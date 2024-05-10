c681565230df723c651f1a573a78d624f11089c9
wkukielk <01187243@pw.edu.pl>
2024-05-10 20:09:45 +0200

    rewritten tests, Stay requires fixing

--- tests/test_timetable_entry.cpp
+++ tests/test_timetable_entry.cpp
@@ -5,43 +5,43 @@
 #include "../src/workers/maid.hpp"
 
 
-TEST_CASE("Test TimetableEntry init")
-{
-    jed_utils::datetime date{ 2024, 4, 11, 2, 32, 55};
-    jed_utils::datetime expected_date{ 2024, 4, 11 };
-    jed_utils::datetime expected_start{ 2024, 4, 11, 22};
-    jed_utils::datetime expected_end{ 2024, 4, 12, 6};
-    TimeInterval expected_interval{expected_start, expected_end};
-    Pay pay{PaycheckMethod::Salary};
-    Receptionist receptionist{"name", "id", pay};
-    TimetableEntry entry{receptionist, date, Shift::III};
-    REQUIRE( entry.get_date() == expected_date );
-    REQUIRE( entry.get_worker() == receptionist );
-    REQUIRE( entry.get_shift() == Shift::III );
-    REQUIRE( entry.get_interval() == expected_interval );
-}
-
-
-TEST_CASE("Test TimetableEntry init invalid")
+TEST_CASE("Test TimetableEntry")
 {
     jed_utils::datetime date{ 2024, 4, 11 };
     Pay pay{PaycheckMethod::Salary};
-    Maid maid{"name", "id", pay};
-    // maids work 2 shifts
-    REQUIRE_THROWS( TimetableEntry{maid, date, Shift::III} );
-}
+    Receptionist receptionist{"name", "id", pay};
 
+    SECTION("init")
+    {
+        jed_utils::datetime date_long{ 2024, 4, 11, 2, 32, 55};
+        jed_utils::datetime expected_start{ 2024, 4, 11, 22};
+        jed_utils::datetime expected_end{ 2024, 4, 12, 6};
+        TimetableEntry entry{receptionist, date_long, Shift::III};
+        REQUIRE( entry.get_date() == date );
+        REQUIRE( entry.get_worker() == receptionist );
+        REQUIRE( entry.get_shift() == Shift::III );
+        TimeInterval expected_interval{expected_start, expected_end};
+        REQUIRE( entry.get_interval() == expected_interval );
+    }
 
-TEST_CASE("Test TimetableEntry operator==")
-{
-    jed_utils::datetime date1{ 2024, 4, 11 };
-    jed_utils::datetime date2{ 2024, 4, 11 };
-    Pay pay{PaycheckMethod::Salary};
-    Receptionist receptionist1{"name1", "id", pay};
-    Receptionist receptionist2{"name2", "id", pay};
-    TimetableEntry entry1{receptionist1, date1, Shift::II};
-    TimetableEntry entry2{receptionist2, date2, Shift::II};
-    REQUIRE( entry1 == entry2 );
-    TimetableEntry entry3 = entry1;
-    REQUIRE( entry1 == entry3 );
+    SECTION("init, invalid, wrong shift")
+    {
+        Maid maid{"name", "id", pay};
+        // maids work 2 shifts
+        REQUIRE_THROWS( TimetableEntry{maid, date, Shift::III} );
+    }
+
+    SECTION("init, invalid, wrong shift")
+    {
+        jed_utils::datetime date1{ 2024, 4, 11 };
+        jed_utils::datetime date2{ 2024, 4, 11 };
+        Pay pay{PaycheckMethod::Salary};
+        Receptionist receptionist1{"name1", "id", pay};
+        Receptionist receptionist2{"name2", "id", pay};
+        TimetableEntry entry1{receptionist1, date1, Shift::II};
+        TimetableEntry entry2{receptionist2, date2, Shift::II};
+        REQUIRE( entry1 == entry2 );
+        TimetableEntry entry3 = entry1;
+        REQUIRE( entry1 == entry3 );
+    }
 }
