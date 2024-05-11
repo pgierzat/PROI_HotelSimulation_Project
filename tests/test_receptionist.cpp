8cdea2f74b1e047b7d10a615344f2b869bff5517
wkukielk <01187243@pw.edu.pl>
2024-05-11 11:19:48 +0200

    rewriting tests, continuation...

---
+++ tests/test_receptionist.cpp
@@ -0,0 +1,76 @@
+#include "catch_amalgamated.hpp"
+#include "../src/workers/receptionist.hpp"
+#include "../src/types/pay.hpp"
+
+
+TEST_CASE("Test Receptionist")
+{
+    Amount salary_amount{3200, 0};
+    Pay salary{PaycheckMethod::Salary, salary_amount};
+    Amount wage_amount{25, 0};
+    Pay wage{PaycheckMethod::Wage, wage_amount};
+
+    SECTION("Regular interface")
+    {
+        Receptionist receptionist_obj{"name1", "1111", salary};
+        Receptionist* receptionist = &receptionist_obj;
+
+        SECTION("init")
+        {
+            REQUIRE( receptionist -> get_type() == WorkerType::Receptionist );
+        }
+    }
+
+    SECTION("Paycheck interface")
+    {
+        SECTION("regular")
+        {
+            Receptionist receptionist_obj{"name1", "1111", salary};
+            Receptionist* receptionist = &receptionist_obj;
+
+            SECTION("init")
+            {
+                REQUIRE( receptionist -> get_complaints() == 0 );
+            }
+
+            SECTION("setters")
+            {
+                receptionist -> set_complaints(10);
+                REQUIRE( receptionist -> get_complaints() == 10 );
+            }
+        }
+
+        SECTION("bonus")
+        {
+            Receptionist receptionist_obj{"name1", "1111", salary};
+            Receptionist* receptionist = &receptionist_obj;
+
+            receptionist -> set_complaints(10);
+            REQUIRE( receptionist -> calculate_paycheck() ==
+                receptionist -> calculate_base_paycheck() - Receptionist::penalty * 10 );
+        }
+
+        SECTION("hours + bonus")
+        {
+            Receptionist receptionist_obj{"name1", "1111", wage};
+            Receptionist* receptionist = &receptionist_obj;
+
+            receptionist -> set_hours_worked(15);
+            receptionist -> set_complaints(10);
+            REQUIRE( receptionist -> calculate_paycheck() ==
+                wage.get_wage() * 15 - Receptionist::penalty * 10 );
+        }
+    }
+
+    SECTION("TimeTable interface")
+    {
+        Receptionist receptionist_obj{"name1", "1111", salary};
+        Worker* receptionist = &receptionist_obj;
+
+        REQUIRE( receptionist -> get_shifts() == 3 );
+        REQUIRE( receptionist -> get_shift_start(Shift::I) == jed_utils::timespan{0, 6} );
+        REQUIRE( receptionist -> get_shift_start(Shift::II) == jed_utils::timespan{0, 14} );
+        REQUIRE( receptionist -> get_shift_start(Shift::III) == jed_utils::timespan{0, 22} );
+        REQUIRE( receptionist -> get_shift_duration() == jed_utils::timespan{0, 8} );
+    }
+}
--- tests/test_worker.cpp
+++ tests/test_worker.cpp
@@ -92,36 +92,3 @@ TEST_CASE("Test Worker")
         }
     }
 }
-
-
-TEST_CASE("Test Maid, interface")
-{
-    Maid maid_obj{"maid", "123", Pay{PaycheckMethod::Salary, Amount{3200, 0}}};
-    Worker* maid = &maid_obj;
-    REQUIRE( maid -> get_shifts() == 2 );
-    REQUIRE( maid -> get_shift_start(Shift::I) == jed_utils::timespan{0, 8} );
-    REQUIRE( maid -> get_shift_start(Shift::II) == jed_utils::timespan{0, 14} );
-    REQUIRE_THROWS( maid -> get_shift_start(Shift::III) );
-    REQUIRE( maid -> get_shift_duration() == jed_utils::timespan{0, 6} );
-}
-
-TEST_CASE("Test Receptionist, interface")
-{
-    Receptionist receptionist_obj{"receptionist", "123", Pay{PaycheckMethod::Salary, Amount{3200, 0}}};
-    Worker* receptionist = &receptionist_obj;
-    REQUIRE( receptionist -> get_shifts() == 3 );
-    REQUIRE( receptionist -> get_shift_start(Shift::I) == jed_utils::timespan{0, 6} );
-    REQUIRE( receptionist -> get_shift_start(Shift::II) == jed_utils::timespan{0, 14} );
-    REQUIRE( receptionist -> get_shift_start(Shift::III) == jed_utils::timespan{0, 22} );
-    REQUIRE( receptionist -> get_shift_duration() == jed_utils::timespan{0, 8} );
-}
-
-TEST_CASE("Test Waiter, interface")
-{
-    Waiter waiter_obj{"waiter", "123", Pay{PaycheckMethod::Salary, Amount{3200, 0}}};
-    Worker* waiter = &waiter_obj;
-    REQUIRE( waiter -> get_shifts() == 1 );
-    REQUIRE( waiter -> get_shift_start(Shift::I) == jed_utils::timespan{0, 8} );
-    REQUIRE_THROWS( waiter -> get_shift_start(Shift::II) );
-    REQUIRE( waiter -> get_shift_duration() == jed_utils::timespan{0, 12} );
-}
