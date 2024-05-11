1161ca2d469d6ce9b5ddc40c382c1ee0df834fef
wkukielk <01187243@pw.edu.pl>
2024-05-11 13:18:21 +0200

    Clock::unit_delta get and set

--- src/systems/clock.cpp
+++ src/systems/clock.cpp
@@ -5,7 +5,11 @@ explicit Clock::Clock(jed_utils::timespan unit_delta) : unit_delta{unit_delta} {
 
 jed_utils::datetime Clock::get_time() const noexcept { return time; }
 
+jed_utils::timespan Clock::get_unit_delta() const noexcept { return unit_delta; }
+
 void Clock::set_time(const jed_utils::datetime& time) { this -> time = time; }
+
+void Clock::set_unit_delta(const jed_utils::timespan& unit_delta) { this -> unit_delta = unit_delta; }
 
 Clock& Clock::operator+=(const jed_utils::timespan& delta)
 {
--- src/systems/clock.hpp
+++ src/systems/clock.hpp
@@ -10,7 +10,9 @@ class Clock
         explicit Clock(jed_utils::timespan unit_delta);
         Clock() = default;
         jed_utils::datetime get_time() const noexcept;
+        jed_utils::timespan get_unit_delta() const noexcept;
         void set_time(const jed_utils::datetime&);
+        void set_unit_delta(const jed_utils::timespan&);
         Clock& operator+=(const jed_utils::timespan&);
         Clock& operator++();
         Clock operator++(int);
