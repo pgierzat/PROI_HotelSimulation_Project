6847b76a1901097f6a2ece7725b90f2d51e2a846
wkukielk <01187243@pw.edu.pl>
2024-06-06 19:42:39 +0200

    clock is a time publisher now

--- src/systems/clock.hpp
+++ src/systems/clock.hpp
@@ -2,25 +2,24 @@
 #define CLOCK_HPP
 
 #include "../types/datetime.h"
+#include "../auxiliary/time_publisher.hpp"
 
 
-class Clock
+class Clock : public TimePublisher
 {
     public:
-        explicit Clock(jed_utils::timespan unit_delta);
-        Clock() = default;
+        explicit Clock(jed_utils::datetime init_time);
         const jed_utils::datetime& get_time() const noexcept;
         jed_utils::timespan get_unit_delta() const noexcept;
         void set_time(const jed_utils::datetime&);
         void set_unit_delta(const jed_utils::timespan&);
         Clock& operator+=(const jed_utils::timespan&);
         Clock& operator++();
+        void publish(const jed_utils::datetime&) const;
         bool operator==(const Clock&);
     private:
         jed_utils::datetime time{1970, 1, 1};
         jed_utils::timespan unit_delta{1};
 };
-
-Clock operator++(Clock&, int);
 
 #endif
