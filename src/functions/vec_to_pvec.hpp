f3bbbfd8d47a7a1b1b84c07937651159d823ad23
wkukielk <01187243@pw.edu.pl>
2024-06-06 21:16:39 +0200

    vec_to_pvec for unique ptrs

--- src/functions/vec_to_pvec.hpp
+++ src/functions/vec_to_pvec.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <ranges>
+#include <memory>
 
 template<typename T>
 std::vector<const T*> vec_to_pvec(const std::vector<T>& vec)
@@ -14,6 +15,14 @@ std::vector<T*> vec_to_pvec(std::vector<T>& vec)
 {
     std::vector<T*> pvec{};
     std::ranges::for_each(vec, [&](const T& elem){ pvec.push_back(&elem); });
+    return pvec;
+}
+
+template<typename T>
+std::vector<const T*> vec_to_pvec(const std::vector<std::unique_ptr<T>>& vec)
+{
+    std::vector<const T*> pvec{};
+    std::ranges::for_each(vec, [&](const auto& elem){ pvec.push_back(elem.get()); });
     return pvec;
 }
 
