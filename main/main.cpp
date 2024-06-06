140434177091e11a87d97e117f06205648d4eaae
wkukielk <01187243@pw.edu.pl>
2024-06-06 18:41:43 +0200

    moved main to proper location

---
+++ main/main.cpp
@@ -0,0 +1,29 @@
+#include "../src/hotel_io/hotel_io.hpp"
+#include "../src/simulation/simulator.hpp"
+#include <fstream>
+#include <memory>
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <workers.json> <rooms.json>\n";
+        return 1;
+    }
+
+    auto workers = HotelIO::read_workers(argv[1]);
+    auto rooms = HotelIO::read_rooms(argv[2]);
+
+    HotelSystem hotel_system;
+    auto& w_system = hotel_system.get_w_system();
+    for (const auto& worker : workers)
+        w_system.add_worker(*worker);
+    auto& rooms_list = hotel_system.get_rooms_list();
+    for (const auto& room : rooms )
+        rooms_list.add_room(*room);
+
+    Simulator simulator(hotel_system);
+    simulator.simulate();
+
+    return 0;
+}
