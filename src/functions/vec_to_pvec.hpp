d4b1c14fcc34087b951531279961d9c0177de882
wkukielk <01187243@pw.edu.pl>
2024-06-07 17:13:12 +0200

    vec to pvec for casting to base class type

--- src/functions/vec_to_pvec.hpp
+++ src/functions/vec_to_pvec.hpp
@@ -1,3 +1,6 @@
+#ifndef VEC_TO_PVEC_HPP
+#define VEC_TO_PVEC_HPP
+
 #include <vector>
 #include <ranges>
 #include <memory>
@@ -21,8 +24,21 @@ std::vector<T*> vec_to_pvec(std::vector<T>& vec)
 template<typename T>
 std::vector<const T*> vec_to_pvec(const std::vector<std::unique_ptr<T>>& vec)
 {
-    std::vector<const T*> pvec{};
-    std::ranges::for_each(vec, [&](const auto& elem){ pvec.push_back(elem.get()); });
+    auto pvec = std::vector<const T*>{};
+    std::ranges::for_each(vec, [&](const auto& elem){ pvec. push_back(elem.get()); });
     return pvec;
 }
 
+
+template<typename T, typename U>
+std::vector<const U*> vec_to_pvec(const std::vector<std::unique_ptr<T>>& vec)
+{
+    std::vector<const U*> pvec{};
+    for (const auto& elem : vec)
+    {
+        pvec.push_back(elem.get());
+    }
+    return pvec;
+}
+
+#endif
