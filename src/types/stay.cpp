6e5de688b856f127e90fad890d90e62c5a43b872
wkukielk <01187243@pw.edu.pl>
2024-06-07 17:14:52 +0200

    stay is also a mosobserver<guest>

--- src/types/stay.cpp
+++ src/types/stay.cpp
@@ -12,9 +12,9 @@ const jed_utils::timespan Stay::checkin_time = jed_utils::timespan{0, 16, 0, 0};
 
 Stay::Stay(const std::string& id, const Room& room, const Guest& main_guest,
            const jed_utils::datetime& start, const jed_utils::datetime& end) :
-    id{id}, room_observer{room}, main_guest_id{main_guest.get_id()}, start{start}, end{end}
+    RoomObs{room}, id{id}, main_guest_id{main_guest.get_id()}, start{start}, end{end}
 {
-    guest_observers.emplace_back(main_guest);
+    GuestsObs::add_observed(main_guest);
     (this -> start).trunkate();
     (this -> end).trunkate();
     validate_duration(start, end);
@@ -24,54 +24,18 @@ const std::string& Stay::get_id() const noexcept { return id; }
 
 const std::vector<const Guest*> Stay::get_guests() const
 {
-    std::vector<const Guest*> guests;
-    for (const auto& observer : guest_observers)
-        guests.push_back(&observer.get_observed());
-    return guests;
+    return GuestsObs::get();
 }
-
-std::vector<const std::string*> Stay::get_guest_ids() const noexcept
-{
-    std::vector<const std::string*> guests;
-    for (const auto& observer : guest_observers)
-        guests.push_back(&observer.get_observed_id());
-    return guests;
-}
-
-std::vector<OwnSystemObserver<Guest>*> Stay::get_guest_observers() noexcept
-{
-    std::vector<OwnSystemObserver<Guest>*> gobs;
-    for (auto& observer : guest_observers)
-        gobs.push_back(&observer);
-    return gobs;
-}
-
-OwnSystemObserver<Guest>& Stay::get_guest_observer(const std::string& id)
-{
-    auto p = std::ranges::find_if(guest_observers,
-        [&](const auto& obs){ return obs.get_observed_id() == id; });
-    if (p == guest_observers.end())
-        throw std::invalid_argument("Guest observer not found among Stay's observers.");
-    return *p;
-}
-
-const std::string& Stay::get_main_guest_id() const noexcept { return main_guest_id; }
 
 const Guest& Stay::get_main_guest() const
 {
-    auto p = std::ranges::find_if(guest_observers,
-        [&](const auto& obs){ return obs.get_observed().get_id() == main_guest_id; });
-    return p -> get_observed();
+    return GuestsObs::get_by_id(main_guest_id);
 }
-
-const std::string& Stay::get_room_id() const noexcept { return room_observer.get_observed_id(); }
 
 const Room& Stay::get_room() const
 {
-    return room_observer.get_observed();
+    return RoomObs::get();
 }
-
-OwnSystemObserver<Room>& Stay::get_room_observer() noexcept { return room_observer; }
 
 jed_utils::datetime Stay::get_start() const noexcept { return start; }
 
@@ -91,14 +55,10 @@ StayStatus Stay::get_status() const noexcept { return status; }
 void Stay::add_guest(const Guest& guest)
 {
     const auto& id = guest.get_id();
-    auto room = get_room();
-    if (guest_observers.size() + 1 > room.getCapacity())
+    auto& room = get_room();
+    if (GuestsObs::size() + 1 > room.getCapacity())
         throw RoomCapacityExceededError("Tried to add 'na waleta' Guest", *this);
-    auto p = std::ranges::find_if(guest_observers,
-        [&](const auto& obs){ return obs.get_observed().get_id() == id; });
-    if (p != guest_observers.end())
-        throw std::invalid_argument("Tried to add duplicate Guest to a Stay.");
-    guest_observers.emplace_back(guest);
+    GuestsObs::add_observed(guest);
 }
 
 void Stay::remove_guest(const Guest& guest)
@@ -106,7 +66,7 @@ void Stay::remove_guest(const Guest& guest)
     const auto& id = guest.get_id();
     if (id == main_guest_id)
         throw std::invalid_argument("Cannot remove main Guest from Stay.");
-    std::erase_if(guest_observers, [&](const auto& obs){ return obs.get_observed_id() == id; });
+    GuestsObs::remove_observed(guest);
 }
 
 void Stay::set_start(const jed_utils::datetime start)
@@ -128,7 +88,7 @@ bool Stay::operator==(const Stay& other) const
     return
         id == other.id &&
         main_guest_id == other.main_guest_id &&
-        get_room_id() == other.get_room_id() &&
+        RoomObs::get_id() == other.RoomObs::get_id() &&
         start == other.start &&
         end == other.end;
 }
--- src/types/stay.hpp
+++ src/types/stay.hpp
@@ -9,6 +9,7 @@
 #include "guest.hpp"
 #include "time_interval.hpp"
 #include "../auxiliary/own_system_observer.hpp"
+#include "../auxiliary/multiple_own_system_observer.hpp"
 
 
 
@@ -24,21 +25,17 @@ enum class StayStatus : unsigned {
 };
 
 
-class Stay
+class Stay : protected OwnSystemObserver<Room>, protected MultipleOwnSystemObserver<Guest>
 {
     public:
+            using RoomObs = OwnSystemObserver<Room>;
+            using GuestsObs = MultipleOwnSystemObserver<Guest>;
         Stay(const std::string& id, const Room&, const Guest& main_guest,
             const jed_utils::datetime& start, const jed_utils::datetime& end);
         const std::string& get_id() const noexcept;
         const std::vector<const Guest*> get_guests() const;
-        std::vector<const std::string*> get_guest_ids() const noexcept;
-        std::vector<OwnSystemObserver<Guest>*> get_guest_observers() noexcept;
-        OwnSystemObserver<Guest>& get_guest_observer(const std::string& id);
-        const std::string& get_main_guest_id() const noexcept;
         const Guest& get_main_guest() const;
-        const std::string& get_room_id() const noexcept;
         const Room& get_room() const;
-        OwnSystemObserver<Room>& get_room_observer() noexcept;
         jed_utils::datetime get_start() const noexcept;
         jed_utils::datetime get_end() const noexcept;
         TimeInterval get_interval() const noexcept;
@@ -53,11 +50,9 @@ class Stay
         bool operator==(const Stay&) const;
         static const jed_utils::timespan checkout_time;
         static const jed_utils::timespan checkin_time;
-    private:   
+    protected:
         static void validate_duration(const jed_utils::datetime& start, const jed_utils::datetime& end);
         std::string id;
-        std::vector<OwnSystemObserver<Guest>> guest_observers;
-        OwnSystemObserver<Room> room_observer;
         std::string main_guest_id;    // Guest that pays for everything
         jed_utils::datetime start;
         jed_utils::datetime end;
