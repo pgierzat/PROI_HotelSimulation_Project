4fbe1cf71bd353c0e514959e974b36777b47a301
Wojciech Kukiełka <wojciech.kukielka.stud@pw.edu.pl>
2024-06-08 16:51:35 +0200

    remove looks for matching id, not object (it could lead to segfault)

--- src/systems/timetable_system.cpp
+++ src/systems/timetable_system.cpp
@@ -15,6 +15,7 @@ const jed_utils::timespan TimetableSystem::minimal_break = jed_utils::timespan{0
 TimetableSystem::TimetableSystem(TimePublisher& t_publisher, WorkerSystem& w_system) :
     time{t_publisher.get_time()}, w_system{&w_system}
 {
+    t_publisher.subscribe(*this);
     w_system.subscribe(*this);
 }
 
@@ -27,18 +28,31 @@ void TimetableSystem::notify(const jed_utils::datetime& time)
     this -> time = time;
 }
 
-void TimetableSystem::add_entry(const TimetableEntry& entry)
+void TimetableSystem::add_entry(const TTEntry& entry)
 {
     if (entry.get_start() < time)
         throw EntryScheduleError("Tried to add a TimetableEntry that has already started.", entry);
     if (not check_minimal_break(entry))
         throw MinimalBreakError("This worker must have an 11-hour's break between shifts.", entry);
-    entries.push_back(std::make_unique<TimetableEntry>(entry));
+    entries.emplace_back(std::make_unique<ITTEntry>(entry));
+    auto& stay_obj = *entries.back();
+    OwnSystemObserver<Worker>& worker_observer = stay_obj.get_worker_observer();
+    try {
+        auto worker_id = worker_observer.get_id();
+        worker_observer.notify_realloc(w_system -> get_by_id(worker_id));
+    } catch (const RoomNotInSystemError& e) {
+        entries.pop_back();
+        throw e;
+    } catch (const GuestNotInSystemError& e) {
+        entries.pop_back();
+        throw e;
+    }
 }
 
-void TimetableSystem::remove_entry(const TimetableEntry& entry)
+void TimetableSystem::remove_entry(const TTEntry& entry)
 {
-    std::erase_if(entries, [&](const auto& otr){ return *otr == entry; });
+    auto id = entry.get_id();
+    std::erase_if(entries, [&](const auto& otr){ return otr -> get_id() == id; });
 }
 
 std::optional<const TimetableEntry*> TimetableSystem::find_by_id(const std::string& id) const
@@ -57,9 +71,11 @@ const TimetableEntry& TimetableSystem::get_by_id(const std::string& id) const
     return **p;
 }
 
-std::vector<const TimetableEntry*> TimetableSystem::get_entries() const noexcept { return vec_to_pvec(entries); }
+std::vector<const TimetableEntry*> TimetableSystem::get_entries() const noexcept {
+    return vec_to_pvec<ITTEntry, TTEntry>(entries);
+}
 
-EntryStatus TimetableSystem::get_entry_status(const TimetableEntry& entry) const noexcept
+EntryStatus TimetableSystem::get_entry_status(const TTEntry& entry) const noexcept
 {
     if (time < entry.get_start())
         return EntryStatus::scheduled;
@@ -88,7 +104,7 @@ void TimetableSystem::notify_erase(const std::string& erased_obj_id, dummy<Worke
 
 bool TimetableSystem::check_minimal_break(const TimetableEntry& entry)
 {
-    auto worker_entries = std::ranges::filter_view(entries, SameWorkerID(entry.get_worker()));
+    auto worker_entries = entries | std::views::filter(SameWorkerID(entry.get_worker()));
     auto p = std::ranges::find_if(worker_entries, TooShortBreak{entry.get_interval(), minimal_break});
     return p == worker_entries.end();
 }
--- src/systems/timetable_system.hpp
+++ src/systems/timetable_system.hpp
@@ -5,7 +5,7 @@
 #include "../auxiliary/time_publisher.hpp"
 #include "../auxiliary/other_system_observer.hpp"
 #include "../utilities/useful.hpp"
-#include "../types/timetable_entry.hpp"
+#include "../inner_types/inner_timetable_entry.hpp"
 
 #ifndef TIMETABLE_SYSTEM_HPP
 #define TIMETABLE_SYSTEM_HPP
@@ -21,26 +21,28 @@ enum class EntryStatus {
 
 class TimetableSystem : public TimeObserver, public OtherSystemObserver<Worker>
 {
+        using TTEntry = TimetableEntry;
+        using ITTEntry = InnerTimetableEntry;
     public:
         TimetableSystem(TimePublisher&, WorkerSystem&);
         TimetableSystem(const TimetableSystem&) = delete;
         const jed_utils::datetime& get_time() const noexcept;
         void notify(const jed_utils::datetime&) override;
-        void add_entry(const TimetableEntry&);
-        void remove_entry(const TimetableEntry&);
-        std::optional<const TimetableEntry*> find_by_id(const std::string&) const;
-        const TimetableEntry& get_by_id(const std::string&) const;
-        std::vector<const TimetableEntry*> get_entries() const noexcept;
-        EntryStatus get_entry_status(const TimetableEntry&) const noexcept;
+        void add_entry(const TTEntry&);
+        void remove_entry(const TTEntry&);
+        std::optional<const TTEntry*> find_by_id(const std::string&) const;
+        const TTEntry& get_by_id(const std::string&) const;
+        std::vector<const TTEntry*> get_entries() const noexcept;
+        EntryStatus get_entry_status(const TTEntry&) const noexcept;
         std::vector<const Worker*> workers_available() const noexcept;
         void notify_realloc(dummy<Worker>);
         void notify_erase(const std::string& erased_obj_id, dummy<Worker>);
-        bool check_minimal_break(const TimetableEntry&);
+        bool check_minimal_break(const TTEntry&);
         static const jed_utils::timespan minimal_break;
     private:
         jed_utils::datetime time{1970, 1, 1};
         WorkerSystem* w_system = nullptr;
-        std::vector<std::unique_ptr<TimetableEntry>> entries;
+        std::vector<std::unique_ptr<ITTEntry>> entries;
 };
 
 #endif
