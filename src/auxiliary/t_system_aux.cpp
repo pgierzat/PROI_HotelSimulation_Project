fd6d010e727ab91ef186eed91466f92eeeb529e0
wkukielk <01187243@pw.edu.pl>
2024-06-11 22:21:18 +0200

    t_system auxiliary predicates

---
+++ src/auxiliary/t_system_aux.cpp
@@ -0,0 +1,29 @@
+#include "t_system_aux.hpp"
+
+TaskSameWorker::TaskSameWorker(const std::string& id) : id{id} {}
+
+bool TaskSameWorker::operator()(const InnerTask& task) const noexcept
+{
+    auto& wosow = task.get_wosow();
+    if (wosow.get_id() == id)
+        return true;
+    auto& wmosow = task.get_wmosow();
+    for (auto pid : wmosow.get_ids())
+        if (*pid == id)
+            return true;
+    return false;
+}
+
+TaskSameRoom::TaskSameRoom(const std::string& id) : id{id} {}
+
+bool TaskSameRoom::operator()(const InnerTask& task) const noexcept {
+    auto& wosor = task.get_wosor();
+    return wosor.get_id() == id;
+}
+
+TaskSameGuest::TaskSameGuest(const std::string& id) : id{id} {}
+
+bool TaskSameGuest::operator()(const InnerTask& task) const noexcept {
+    auto& wosog = task.get_wosog();
+    return wosog.get_id() == id;
+}
