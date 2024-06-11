9ae27d1c5d1b524a10bbf98f3c3d5055ad02ee8f
wkukielk <01187243@pw.edu.pl>
2024-06-11 12:01:53 +0200

    tested stay's new interface

--- tests/test_stay.cpp
+++ tests/test_stay.cpp
@@ -11,35 +11,39 @@
 TEST_CASE("Test Stay")
 {
     auto g_system = GuestSystem{};
-    g_system.add_guest(Guest{"id1", "name1"});
-    g_system.add_guest(Guest{"id2", "name2"});
-    g_system.add_guest(Guest{"id3", "name3"});
-    auto& guest1 = g_system.get_by_id("id1");
-    auto& guest2 = g_system.get_by_id("id2");
-    auto& guest3 = g_system.get_by_id("id3");
+    auto guest1 = Guest{"id1", "name1"};
+    auto guest2 = Guest{"id2", "name2"};
+    auto guest3 = Guest{"id3", "name3"};
     auto rooms_list = RoomsList{};
-    rooms_list.add_room(TwoRoom{"237"});
-    const auto& room1 = rooms_list.get_by_id("237");
+    auto room1 = TwoRoom{"237"};
     jed_utils::datetime start{2012, 12, 12};
     jed_utils::datetime end{2012, 12, 14};
     auto stay = Stay{"id1", room1, guest1, start, end};
 
     SECTION("init")
     {
-        jed_utils::datetime start_long{2012, 12, 12, 6, 15, 5};
-        jed_utils::datetime end_long{2012, 12, 14, 3, 12, 7};
+        
 
         SECTION("regular")
         {
-            std::vector<Guest> exp{guest1};
+            std::vector<const Guest*> exp{&guest1};
             REQUIRE( stay.get_guests() == exp );
             REQUIRE( stay.get_main_guest() == guest1 );
             REQUIRE( stay.get_room() == room1 );
-            REQUIRE( stay.get_start() == jed_utils::datetime{2012, 12, 12} );
-            REQUIRE( stay.get_end() == jed_utils::datetime{2012, 12, 14} );
+            REQUIRE( stay.get_start() == start );
+            REQUIRE( stay.get_end() == end );
             auto interval = stay.get_interval();
             REQUIRE( interval.get_start() == start + Stay::checkin_time);
             REQUIRE( interval.get_end() == end + Stay::checkout_time);
+        }
+
+        SECTION("long dates")
+        {
+            jed_utils::datetime start_long{2012, 12, 12, 6, 15, 5};
+            jed_utils::datetime end_long{2012, 12, 14, 3, 12, 7};
+            auto stay1 = Stay{"id1", room1, guest1, start_long, end_long};
+            REQUIRE( stay.get_start() == start );
+            REQUIRE( stay.get_end() == end );
         }
 
         SECTION("setters")
@@ -49,10 +53,18 @@ TEST_CASE("Test Stay")
             jed_utils::datetime nend{2012, 12, 16};
             stay2.set_end(nend);
             stay2.set_start(nstart);
-            stay2.set_status(StayStatus::checked_out);
             REQUIRE( stay2.get_start() == nstart );
             REQUIRE( stay2.get_end() == nend );
-            REQUIRE( stay2.get_status() == StayStatus::checked_out );
+        }
+
+        SECTION("setting status freely")
+        {
+            stay.set_status(StayStatus::checked_out);
+            REQUIRE( stay.get_status() == StayStatus::checked_out );
+            stay.set_status(StayStatus::booked);
+            REQUIRE( stay.get_status() == StayStatus::booked );
+            stay.set_status(StayStatus::checked_in);
+            REQUIRE( stay.get_status() == StayStatus::checked_in );
         }
 
         SECTION("setters, invalid, end before start")
@@ -77,19 +89,13 @@ TEST_CASE("Test Stay")
         SECTION("regular")
         {
             stay.add_guest(guest2);
-            std::vector exp_guests{guest1, guest2};
+            std::vector<const Guest*> exp_guests{&guest1, &guest2};
             REQUIRE( stay.get_guests() == exp_guests );
         }
 
         SECTION("duplicate guest")
         {
             REQUIRE_THROWS( stay.add_guest(guest1) );
-        }
-
-        SECTION("unknown guest")
-        {
-            auto guest4 = Guest{"id4", "name4"};
-            REQUIRE_NOTHROW( stay.add_guest(guest4) );
         }
 
         SECTION("'na waleta' guest")
