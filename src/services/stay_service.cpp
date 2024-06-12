daa858d86f0f3eb50685be1013ae0882846c0fb6
wkukielk <01187243@pw.edu.pl>
2024-06-12 14:11:48 +0200

    stay service observes stay

--- src/services/stay_service.cpp
+++ src/services/stay_service.cpp
@@ -3,47 +3,21 @@
 
 const std::string StayService::description = "Stay Service.";
 
-StaySystem* StayService::s_system = nullptr;
-
 StayService::StayService(const std::string& id, const Stay& stay) :
-    Service{id, stay.get_main_guest()}, stay_id{stay.get_id()} {}
+    Service{id, stay.get_main_guest()}, OSO{stay}
+{
+    price = get_default_price();
+}
 
 Amount StayService::get_default_price() const noexcept
 {
-    const auto& stay = get_stay();
+    const auto& stay = OSO::get();
     const auto& room = stay.get_room();
     return room.calculatePrice() * stay.get_duration();
 }
 
-const Stay& StayService::get_stay() const noexcept
-{
-    return s_system -> get_by_id(stay_id);
-}
+const Stay& StayService::get_stay() const noexcept { return OSO::get(); }
 
 const std::string& StayService::get_description() const noexcept { return description; }
 
-ServiceStatus StayService::refresh_status(const ServiceSystem& sc_system)
-{
-    if (status >= ServiceStatus::completed)
-        return status;
-    StayStatus s_status = s_system -> get_by_id(stay_id).get_status();
-    switch (s_status)
-    {
-        case StayStatus::booked:
-            status = ServiceStatus::ordered;
-            break;
-        case StayStatus::checked_in:
-            status = ServiceStatus::in_progress;
-            break;
-        case StayStatus::checked_out:
-            status = ServiceStatus::completed;
-            break;
-        default:
-            throw UnknownStayStatusError{"cannot refresh StayService's state.", s_status};
-    }
-    return status;
-}
-
 void StayService::add_to_systems(ServiceSystem& sc_system) {}
-
-void StayService::set_s_system(StaySystem& s_system) { StayService::s_system = &s_system; }
--- src/services/stay_service.hpp
+++ src/services/stay_service.hpp
@@ -4,21 +4,19 @@
 #include "service.hpp"
 #include "../types/stay.hpp"
 
-class StayService : public Service
+class StayService : public Service, protected OwnSystemObserver<Stay>
 {
+        using OSO = OwnSystemObserver<Stay>;
     public:
         StayService(const std::string& id, const Stay&);
         Amount get_default_price() const noexcept override;
         const Stay& get_stay() const noexcept;
         const std::string& get_description() const noexcept override;
-        ServiceStatus refresh_status(const ServiceSystem&) override;
         void add_to_systems(ServiceSystem&) override;
-        static void set_s_system(StaySystem& s_system);
     private:
-        static StaySystem* s_system;
         static const std::string description;
         Amount price;
-        std::string stay_id;
+        const Stay* stay;
 };
 
 #endif
