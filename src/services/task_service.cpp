22790b0f5ea89ff228ae6794bc28c2ac768c4f8c
wkukielk <01187243@pw.edu.pl>
2024-06-12 14:26:36 +0200

    new task service

--- src/services/task_service.cpp
+++ src/services/task_service.cpp
@@ -2,38 +2,7 @@
 #include "../systems/task_system.hpp"
 
 
-TaskSystem* TaskService::t_system = nullptr;
-
 TaskService::TaskService(const std::string& id, const Guest& requestee) :
-    Service{id, requestee} {}
+    Service{id, requestee}, MOSO{} {}
 
-ServiceStatus TaskService::refresh_status(const ServiceSystem& sc_system) noexcept
-{
-    if (status >= ServiceStatus::completed)
-        return status;
-    status = ServiceStatus::completed;
-    for (auto task : get_tasks())
-    {
-        if (status == ServiceStatus::completed)
-        {
-            if (task -> get_status() == TaskStatus::assigned)
-                status = ServiceStatus::in_progress;
-            else if (task -> get_status() == TaskStatus::unassigned)
-            {
-                status = ServiceStatus::ordered;
-                break;
-            }
-        }
-        else
-        {
-            if (task -> get_status() == TaskStatus::unassigned)
-            {
-                status = ServiceStatus::ordered;
-                break;
-            }
-        }
-    }
-    return status;
-}
-
-void TaskService::set_t_system(TaskSystem& t_system) { TaskService::t_system = &t_system; }
+std::vector<const Task*> TaskService::get_tasks() const { return MOSO::get(); }
